8b69f7dd888bb262e79f83e05b991d4db0e0f0fb
satyaki95 <www.satyaki95@gmail.com>
2024-03-18 21:56:24 +0530

    Data-Type

---
+++ 2 Variables and Data Types/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static void Main(string[] args){
+
+        string name;
+        name = "Satyaki Saha";
+        Console.WriteLine(name);
+        name = "Guddu";
+        Console.WriteLine(name);
+        int age = 30;
+        Console.WriteLine(age);
+        double gpa = 9.368;
+        Console.WriteLine(gpa);
+        bool isAccept = true;
+        Console.WriteLine(isAccept);
+    }
+}
