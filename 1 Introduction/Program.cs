d601ef887628f8a513ba23e7f25266becb581239
satyaki95 <www.satyaki95@gmail.com>
2024-03-18 20:21:24 +0530

    print bio

--- 1 Introduction/Program.cs
+++ 1 Introduction/Program.cs
@@ -6,5 +6,13 @@ class Test{
         //Console.WriteLine("Hello, World!");
         System.Console.WriteLine("Satyaki Saha");
         System.Console.WriteLine(123);
+
+        string name         = "My Name Is Satyaki Saha.";
+        string age          = "I Am 30 Years Old.";
+        string profetion    = "I Am A Full-Stack Developer.";
+
+        System.Console.WriteLine(name);
+        System.Console.WriteLine(age);
+        System.Console.WriteLine(profetion);
     }
 }
