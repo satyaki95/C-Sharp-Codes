8f1ab4fe8727dddd31ce5f3a469eea3e9d4aa2a8
satyaki95 <www.satyaki95@gmail.com>
2024-03-20 12:31:03 +0530

    Conversion

---
+++ 3 Type Casting/Program.cs
@@ -0,0 +1,24 @@
+﻿class Test{
+    public static void Main(string[] args){
+        double salary = 2700.56;
+        Console.WriteLine(salary);
+
+        int salary2 = (int)salary;
+        Console.WriteLine(salary2);
+
+        // Implicit / Automatic Conversion
+// char -> int -> long -> float -> double ->
+            int number = 2700.56;
+            double number2 = number;
+            Console.WriteLine(number2);
+
+        // Explicit / Manual Conversion
+// double -> float -> long -> int -> char
+
+            int chracter = 2700;
+            
+            Console.WriteLine(Convert.ToString(chracter));
+            Console.WriteLine(Convert.ToDouble(chracter));
+            Console.WriteLine(Convert.ToChar(chracter));
+    }
+}
