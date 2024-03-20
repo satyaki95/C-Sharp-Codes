ef602324ec1ece2b28c880d1b5718580962074ec
satyaki95 <www.satyaki95@gmail.com>
2024-03-20 10:48:15 +0530

    data-type

--- 2 Variables and Data Types/Program.cs
+++ 2 Variables and Data Types/Program.cs
@@ -16,5 +16,20 @@ class Program
         Console.WriteLine(gpa);
         bool isAccept = true;
         Console.WriteLine(isAccept);
+
+
+        //  Assignment
+
+        string Name = "Apple iphone 14";
+        double Price = 320.5;
+        string category = "smart phone";
+        bool Available = true;
+        int sold = 5;
+
+        Console.WriteLine(Name);
+        Console.WriteLine(Price);
+        Console.WriteLine(category);
+        Console.WriteLine(Available);
+        Console.WriteLine(sold);
     }
 }
