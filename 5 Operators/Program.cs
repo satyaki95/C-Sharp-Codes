144e3703b0802882b517f30f3156dbd16d193edc
satyaki95 <www.satyaki95@gmail.com>
2024-03-23 13:00:59 +0530

    operator

---
+++ 5 Operators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args){
+        int result = 25 + 30;
+        Console.WriteLine(result);
+        Console.WriteLine(4*9+6);
+        Console.WriteLine(7500/30);
+
+
+        int number1 = 10;
+        int number2 = 3;
+
+        Console.WriteLine(number1+number2);
+        Console.WriteLine(number1-number2);
+        Console.WriteLine(number1*number2);
+        Console.WriteLine(number1/number2);
+        Console.WriteLine(number1%number2);
+
+    int result2 = number1+number2;
+
+        Console.WriteLine($"{number1} + {number2} = {result2}");
+
+        double div = (double)number1 / number2;
+        // Format Spesifier
+        Console.WriteLine($"{number1} / {number2} = {div.ToString("F2")}");
+    }
+}
