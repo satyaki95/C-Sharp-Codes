faacaadbc4ce88114e0a95734c75e61c29da83cf
satyaki95 <www.satyaki95@gmail.com>
2024-03-25 14:16:38 +0530

    sum of 3 number

---
+++ 6 Sum and Average of 3 Number/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int number1,number2,number3;
+        double sum;
+
+        Console.Write("Enter 1st No = ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter 2nd No = ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter 3rd No = ");
+        number3 = Convert.ToInt32(Console.ReadLine());
+
+        sum = (number1+number2+number3)/3.0;
+        Console.WriteLine($"avg = {sum1}");
+    }
+}
