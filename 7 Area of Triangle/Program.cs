7fe2e264557f5e62f6b0d063c4f07fb58e458354
satyaki95 <www.satyaki95@gmail.com>
2024-03-25 14:38:31 +0530

    Area of Tri

---
+++ 7 Area of Triangle/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(){
+        double baseLength,height,area;
+
+        Console.Write("Enter Base Of Triangle = ");
+        baseLength = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Height Of Triangle = ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        area = 0.5 * baseLength * height;
+
+        Console.WriteLine($"AREA = {area.ToString("F2")}");
+        
+    }
+}
