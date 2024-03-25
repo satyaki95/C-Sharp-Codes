1347a2f5740630aeaa6c1e68400170b6aeea45d6
satyaki95 <www.satyaki95@gmail.com>
2024-03-25 18:34:44 +0530

    Circle Area

---
+++ 8 Area of Circle/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main(){
+        double radious,area;
+        
+        
+        Console.Write("Enter Circle Radious = ");
+        radious = Convert.ToDouble(Console.ReadLine());
+
+        area = Math.PI * radious * radious;
+
+        Console.WriteLine($"Circle Area = {area.ToString("F2")}");
+
+    }
+}
