48240ba92be433884155ad2e58a5577805c97349
satyaki95 <www.satyaki95@gmail.com>
2024-03-25 18:45:01 +0530

    Temperature converter

---
+++ 9 Temperature Conveter/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(){
+
+        Console.WriteLine("F to C Conveter");
+
+        int far;
+        double cel;
+
+        Console.Write("Enter Your F = ");
+        far = Convert.ToInt32(Console.ReadLine());
+
+        cel = (far - 32) / 1.8;
+
+        Console.WriteLine($"C = {cel.ToString("F2")}");
+    }
+}
