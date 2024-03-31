97c8e9bbfa038e19e02ca9eb4da33d9123c44038
satyaki95 <www.satyaki95@gmail.com>
2024-03-31 13:04:01 +0530

    Assignment Operator

---
+++ 10 Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main(string[] args){
+
+        int number1,number2;
+
+        number1 = 20;
+        number2 = 30;
+        // Assignment Operator
+        number1 += number2;
+
+        Console.WriteLine($"{number1}");
+    }
+}
