97b16c544f559dc7b4b32c6ef068fb6daa363f49
satyaki95 <www.satyaki95@gmail.com>
2024-03-31 13:11:54 +0530

    Relation Operator

--- 10 Operators/Program.cs
+++ 10 Operators/Program.cs
@@ -2,13 +2,26 @@
 {
     public static void Main(string[] args){
 
-        int number1,number2;
+        int number1,number2,number3;
 
         number1 = 20;
         number2 = 30;
+        number3 = 50;
         // Assignment Operator
-        number1 += number2;
 
+        number1 += number2;
         Console.WriteLine($"{number1}");
+
+        number1 -= number2;
+        Console.WriteLine($"{number1}");
+
+        // Relation Operator
+
+        Console.WriteLine($"{number1 > number2}");
+        Console.WriteLine($"{number1 >= number2}");
+        Console.WriteLine($"{number1 < number2}");
+        Console.WriteLine($"{number1 <= number2}");
+        Console.WriteLine($"{number1 == number2}");
+        Console.WriteLine($"{number1 != number2}");
     }
 }
