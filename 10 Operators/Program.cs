e0237cba5ce77b9bde24272bbe529198f99210a6
satyaki95 <www.satyaki95@gmail.com>
2024-03-31 18:38:08 +0530

    Operators

--- 10 Operators/Program.cs
+++ 10 Operators/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main(string[] args){
 
-        int number1,number2,number3;
+        int number1,number2,number3,result;
 
         number1 = 20;
         number2 = 30;
@@ -30,5 +30,34 @@
         Console.WriteLine(8 > 9 || 8 < 9 || 13 > 9);
         Console.WriteLine(!(8 > 9 && 8 < 9 && 13 > 9));
 
+        // Birwise Operator
+
+        result = number1 & number2;
+        Console.WriteLine($"bit-and --> {number1} & {number2} = {result}");
+
+        result = number1 | number2;
+        Console.WriteLine($"bit-or --> {number1} | {number2} = {result}");
+
+        result = number1 ^ number2;
+        Console.WriteLine($"bit-xor --> {number1} ^ {number2} = {result}");
+
+        result = number1 >> 2;
+        Console.WriteLine($"bit-right shift --> {number1} >> {2} = {result}");
+
+        result = number1 << 2;
+        Console.WriteLine($"bit-left shift --> {number1} << {2} = {result}");
+
+        // Unary Operator
+
+        Console.WriteLine(number1++);
+        Console.WriteLine(number1);
+        Console.WriteLine(++number1);
+        Console.WriteLine(number1--);
+        Console.WriteLine(--number1);
+
+        // Ternary Operators
+
+        string check = number1>number2 ? "Number1":"number2";
+        Console.WriteLine($"Check = {check}");
     }
 }
