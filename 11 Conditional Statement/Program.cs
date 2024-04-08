e705c837236b1d42487387c463729c3de9076d9c
satyaki95 <www.satyaki95@gmail.com>
2024-04-08 19:23:14 +0530

    if Statement

---
+++ 11 Conditional Statement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args){
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("Positive Number");
+        }
+        else if(number < 0){
+            Console.WriteLine("Negative Number");
+        }
+        else
+        {
+           Console.WriteLine("Zero Number"); 
+        }
+
+    }
+}
