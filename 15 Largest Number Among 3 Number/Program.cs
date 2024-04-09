1a369ff5ca252d165a8409d9d26fac03817c8611
satyaki95 <www.satyaki95@gmail.com>
2024-04-09 19:36:04 +0530

    Largest number among 3 numbers

---
+++ 15 Largest Number Among 3 Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number1,number2,number3;
+
+        Console.Write("Number 1 = ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Number 2 = ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Number 3 = ");
+        number3 = Convert.ToInt32(Console.ReadLine());
+
+        int check = number1 > number2 && number1 > number3 ? number1 : number2 > number3 ? number2 : number3;
+
+        Console.WriteLine($"{check} is Greater Number");
+    }
+}
