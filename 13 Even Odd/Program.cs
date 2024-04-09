168c5cdcc2409713b56f4bfb56372dd58fb66c47
satyaki95 <www.satyaki95@gmail.com>
2024-04-09 19:00:27 +0530

    even odd

---
+++ 13 Even Odd/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args){
+        int number ;
+        Console.Write("Enter A Number = ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if(number/2 == 0){
+            Console.WriteLine($"{number} Is Even Number");
+        }else
+        {
+            Console.WriteLine($"{number} Is Odd Number");
+        }
+}
+}
