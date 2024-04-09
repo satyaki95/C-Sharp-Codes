6eb1de9f523f60cdf37b84c5ce2250e5c816e7fe
satyaki95 <www.satyaki95@gmail.com>
2024-04-09 19:11:38 +0530

    Large Number

---
+++ 14 Large Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args){
+        int number1, number2;
+
+        Console.Write("Enter 2 Number ");
+
+        number1 = Convert.ToInt32(Console.ReadLine());
+        number2 = Convert.ToInt32(Console.ReadLine());
+
+        if(number1 > number2){
+            Console.WriteLine($"{number1} is Gater Then {number2}");
+        }
+        else if(number2 > number1){
+            Console.WriteLine($"{number2} is Gater Then {number1}");
+        }else
+    {
+        Console.WriteLine($"{number1} is equel to {number2}");
+    }
+    }
+}
