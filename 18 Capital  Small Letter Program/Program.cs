b49e023259f584948261a0455af2742725a5f3b6
satyaki95 <www.satyaki95@gmail.com>
2024-04-16 20:16:04 +0530

    Capital  Small Letter Program

---
+++ 18 Capital  Small Letter Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args){
+        char letter;
+
+        Console.Write("Enter Any Letter = ");
+        letter = Convert.ToChar(Console.ReadLine());
+
+        if(letter >= 'A' && letter <= 'Z'){
+            Console.WriteLine($"{letter} is Capital Letter");
+        }else
+        {
+             Console.WriteLine($"{letter} is Small Letter");
+        }
+    }
+}
