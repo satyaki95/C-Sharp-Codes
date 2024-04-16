c153375d13cadb8550a02bc10a00bbc5bc9b8f8d
satyaki95 <www.satyaki95@gmail.com>
2024-04-16 20:20:32 +0530

    Vowel

--- 18 Capital  Small Letter Program/Program.cs
+++ 18 Capital  Small Letter Program/Program.cs
@@ -9,10 +9,21 @@ class Program
         letter = Convert.ToChar(Console.ReadLine());
 
         if(letter >= 'A' && letter <= 'Z'){
-            Console.WriteLine($"{letter} is Capital Letter");
+            if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+            {
+               Console.WriteLine($"{letter} is Capital Letter And Vowel"); 
+            }
+            
         }else
         {
-             Console.WriteLine($"{letter} is Small Letter");
+             if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+               Console.WriteLine($"{letter} is Small Letter And Vowel"); 
+            }else
+            {
+                Console.WriteLine($"{letter} is Small Letter And Consonant");
+            }
+             
         }
     }
 }
