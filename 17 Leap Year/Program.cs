bfd294f3de8f80ea6c63d74aedc46fafb8615677
satyaki95 <www.satyaki95@gmail.com>
2024-04-16 19:52:40 +0530

    Leap Year

---
+++ 17 Leap Year/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(String[] args){
+        int number;
+
+        Console.Write("Enter A Year = ");
+
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if(number % 400 == 0){
+            Console.WriteLine($"{number} Leap Year");
+        }
+        else if(number % 400 == 0 && number % 100 != 0){
+            Console.WriteLine($"{number} Leap Year");
+        }
+        else
+        {
+           Console.WriteLine($"{number} Is Not Leap Year"); 
+        }
+    }
+}
